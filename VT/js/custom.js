d1a064f4a0c91e9dfc9407484c0510de73e46701
parvezrandr <parvej.alam@researchandranking@gmail.com>
2020-05-01 00:06:49 +0530

    asdf

--- VT/js/custom.js
+++ VT/js/custom.js
@@ -118,60 +118,6 @@ $(document).ready(function() {
 // Navbar Button Style End
 
 
-// VideoPlayback 
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#polina button");
-
-if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
-    pauseButton.innerHTML = "Paused";
-}
-
-function vidFade() {
-  vid.classList.add("stopfade");
-}
-
-vid.addEventListener('ended', function()
-{
-// only functional if "loop" is removed 
-vid.pause();
-// to capture IE10
-vidFade();
-}); 
-
-
-$(document).on('click', 'video', function(){
-    var id = $(this).data('id');
-    vid = document.getElementById("bgvid"+id);
-  vid.classList.toggle("stopfade");
-  
-  if (vid.paused) {
-    // vid.play();
-    // $(".polinaPlay"+id).hide()
-    // pauseButton.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    $(".polinaPlay"+id).show()
-    // pauseButton.innerHTML = "play";
-  }
-})
-
-$(document).on('click', '.virtualArea button', function(){
-    var id = $(this).data('id');
-    vid = document.getElementById("bgvid"+id);
-  vid.classList.toggle("stopfade");
-  
-  if (vid.paused) {
-    vid.play();
-    $(".polinaPlay"+id).hide()
-    // pauseButton.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    $(".polinaPlay"+id).show()
-    // pauseButton.innerHTML = "play";
-  }
-})
 
 // VideoPlayback ended
 
