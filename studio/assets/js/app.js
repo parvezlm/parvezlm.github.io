9a8451a1b1cbceab852dc4911d04fd8485c7cdea
parvezlm <51526099+parvezlm@users.noreply.github.com>
2020-12-27 22:34:23 +0530

    changes done

--- studio/assets/js/app.js
+++ studio/assets/js/app.js
@@ -1,3 +1,46 @@
+(function($) {
+
+  // Init empty gallery array
+  var container = [];
+
+  // Loop over gallery items and push it to the array
+  $('#gallery').find('figure').each(function() {
+    var $link = $(this).find('a'),
+      item = {
+        src: $link.attr('href'),
+        w: $link.data('width'),
+        h: $link.data('height'),
+        title: $link.data('caption')
+      };
+    container.push(item);
+  });
+
+  // Define click event on gallery item
+  $('.gallery figure a').click(function(event) {
+
+    // Prevent location change
+    event.preventDefault();
+
+    // Define object and gallery options
+    var $pswp = $('.pswp')[0],
+      options = {
+        index: $(this).parent('figure').index(),
+    bgOpacity: 0.8,
+    captionEl: false,
+    tapToClose: true,
+    shareEl: false,
+    fullscreenEl: false,
+      };
+
+    // Initialize PhotoSwipe
+    var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+    gallery.init();
+  });
+
+}(jQuery));
+
+
+
 $(document).ready(function() {
 	$(window).scroll(function() {
 		if($(this).scrollTop() > 45) {
@@ -92,6 +135,36 @@ $(document).ready(function() {
 	});
 
 
+	 // logo slider
+	 $('.logo_slider').owlCarousel({
+	    loop:true,
+	    margin:20,
+	    item:5,
+	    nav: false,
+        dots:false,
+        autoplay: true,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+                    nav: false,
+                    items: 1,
+                },
+                768:{
+                    nav: true,
+                    items: 2,
+                },
+                980:{
+                    items: 3,
+                },
+                1200:{
+                    items: 5,
+                },
+	    }
+	});
+
+
+
+
 	 // latest work tabs
 	 $('.tab_btn li').click(function() {
 	 	var $this = $(this).attr('data-filter');
@@ -99,8 +172,34 @@ $(document).ready(function() {
 	 	$('.tab_btn li').removeClass('active');
 	 	$(this).addClass('active');
 
-	 	$('.work').fadeOut('fast');
-	 	$('.' + $this).fadeIn();
+	 	$('.latesWork_card').hide();
+	 	$('.' + $this).show();
+	 });
+
+
+	 $('.hamburge_icon').click(function() {
+	 	$(this).toggleClass('open');
+	 	$('.menu_wraper').toggleClass('left');
+	 });
+
+
+	 // accordion
+	 $('.accord_btn').click(function() {
+	 	if($(this).hasClass('active')) {
+	 		$(this).removeClass('active');
+	 		$(this).next('.content').slideUp();
+	 		$(this).find('i').removeClass('fa-minus').addClass('fa-plus');
+	 	}else {
+	 		$('.accord_btn').removeClass('active');
+	 		$(this).addClass('active');
+
+	 		$('.accord_btn').find('i').removeClass('fa-minus').addClass('fa-plus');
+	 		$(this).find('i').removeClass('fa-plus').addClass('fa-minus');
+
+	 		$('.accord_btn').next('.content').slideUp();
+	 		$(this).next('.content').slideDown();
+	 	}
+	 	
 	 })
 
 })
