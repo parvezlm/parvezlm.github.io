1767643fa2480530fdcc02b3c3bd741531ac2547
parvezlm <51526099+parvezlm@users.noreply.github.com>
2021-01-18 15:27:38 +0530

    Update app.js

--- studio/assets/js/app.js
+++ studio/assets/js/app.js
@@ -212,14 +212,7 @@ $(document).ready(function() {
 
 })
 
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-	$(".zoom ").css({
-		transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/5)/100+')',
-		//Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-		//"-webkit-filter": "blur(" + (scroll/200) + "px)",
-		//filter: "blur(" + (scroll/200) + "px)"
-	});
+
 });
 
 
